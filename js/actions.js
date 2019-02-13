7f6008020fc101824d6567317d97b8711c1398e1
Thomas Aylott <taylott@INTERGAGE2.LOCAL>
2019-02-13 13:44:54 +0000

    Possible opacity and positioning changes

--- js/actions.js
+++ js/actions.js
@@ -1,16 +1,63 @@
 (function($) {
+  function Utils() {}
+
+  Utils.prototype = {
+    constructor: Utils,
+    isElementInView: function(element, fullyInView) {
+      var pageTop = $(window).scrollTop();
+      var pageBottom = pageTop + $(window).height();
+      var elementTop = $(element).offset().top;
+      var elementBottom = elementTop + $(element).height();
+
+      if (fullyInView === true) {
+        return pageTop < elementTop && pageBottom > elementBottom;
+      } else {
+        return elementTop <= pageBottom && elementBottom >= pageTop;
+      }
+    }
+  };
+
+  var Utils = new Utils();
+
   function setFooterHeight() {
-    $('body').css("margin-bottom", $("#footer").height());
+    $("body").css("margin-bottom", $("#footer").height());
   }
   setFooterHeight();
+
+  function isInView() {
+    var isElementInView = Utils.isElementInView($(".pp-possible"), false);
+
+    var scrollTop = $(window).scrollTop(),
+      elementOffset = $(".pp-possible").offset().top,
+      distance = elementOffset - scrollTop,
+      height = $(window).height();
+
+    if (isElementInView) {
+      var opac = parseInt(100 - (distance / height) * 100) - 30;
+      console.log(opac / 100);
+      $(".pp-possible__article").css({
+        bottom: 300 - distance + "px"
+      });
+      $(".pp-possible__logo").css({
+        opacity: opac / 80
+      });
+    }
+  }
+  isInView();
+
   //RESIZE STUFF
   var resizeTimer;
+  var scrollTimer;
 
   $(window).on("resize", function(e) {
     clearTimeout(resizeTimer);
     resizeTimer = setTimeout(function() {
       setFooterHeight();
     }, 250);
+  });
+
+  $(window).on("scroll", function(e) {
+    isInView();
   });
 
   $(".pp-carousel__stage").slick({
